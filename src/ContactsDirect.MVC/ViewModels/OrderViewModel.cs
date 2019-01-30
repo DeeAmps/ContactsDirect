d85bec97afe6cd20c841ea19117ba43da7776733
Daniel <daniel.bennin@fasylgh.com>
2019-01-30 12:13:15 +0000

    Part done

---
+++ src/ContactsDirect.MVC/ViewModels/OrderViewModel.cs
@@ -0,0 +1,17 @@
+﻿using ContactsDirect.Data.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContactsDirect.MVC.ViewModels
+{
+    public class OrderViewModel
+    {
+        public List<lenstype> lenstype { get; set; }
+        public List<subscriptionlength> subscriptionlength { get; set; }
+        public List<paymentmethod> paymentmethod { get; set; }
+        public List<prescriptiontype> prescriptiontype { get; set; }
+        public List<optionalproduct> optionalproduct { get; set; }
+    }
+}
---
+++ src/ContactsDirect.Web/Controllers/OrderController.cs
@@ -0,0 +1,34 @@
+﻿using ContactsDirect.Data.DB;
+using ContactsDirect.Web.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//using ContactsDirect.Web.ViewModels;
+
+namespace ContactsDirect.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        private contactsdirectEntities service;
+        public OrderController()
+        {
+            service = new contactsdirectEntities();
+        }
+        // GET: Order
+        public ActionResult Index()
+        {
+            var ordervm = new OrderViewModel()
+            {
+                lenstype = service.lenstypes.ToList(),
+                prescriptiontype = service.prescriptiontypes.ToList(),
+                subscriptionlength = service.subscriptionlengths.ToList(),
+                paymentmethod = service.paymentmethods.ToList(),
+                optionalproduct = service.optionalproducts.ToList(),
+            };
+            return View(ordervm);
+            //return View();
+        }
+    }
+}
